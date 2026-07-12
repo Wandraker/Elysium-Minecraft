ed2b7dfa71a33c79bd6f3571d97c1c4f2ed4d600
Ywq <103017546+Wandraker@users.noreply.github.com>
2026-07-12 05:17:51 +0500

    Delete js directory

--- js/shield-config.js
+++
@@ -1,6 +0,0 @@
-window.ELYSIUM_SHIELD_CONFIG = Object.freeze({
-  enabled: true,
-  apiBase: "https://elysium-shield.ilalebedev171.workers.dev",
-  turnstileSiteKey: "0x4AAAAAAD0A2bKXWH3Dhqr0",
-  verifyEndpoint: "/v1/site/verify"
-});
